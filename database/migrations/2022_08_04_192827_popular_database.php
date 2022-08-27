2ef11f07a7483d8ed6c71cd8d96df2672720897a
Gabriel <gabrieldias122@gmail.com>
2022-08-27 12:32:16 -0300

    populando  crud animais

--- database/migrations/2022_08_04_192827_popular_database.php
+++ database/migrations/2022_08_04_192827_popular_database.php
@@ -43,7 +43,7 @@ class PopularDatabase extends Migration
                     'id' => 1,
                     'nome' => 'Gabriel',
                     'cpf' => '999.999.999-99',
-                    'dt_nasc' => '04/03/1998',
+                    'dt_nasc' => '04-03-1998',
                     'sexo' => 'M',
                     'funcao' => 'Desenvolvedor',
                     'telefone' => '(99)99999-9999',
@@ -76,8 +76,8 @@ class PopularDatabase extends Migration
                 [
                     'id' => 1,
                     'nome' => 'Área norte',
-                    'dt_ini' => '22/03/2021',
-                    'dt_fim' => '04/05/2022',
+                    'dt_ini' => '22-03-2021',
+                    'dt_fim' => '04-05-2022',
                     'tipo' => 'ARRENDADA',
                     'ha' => '12',
                     'util' => '2',
@@ -92,8 +92,8 @@ class PopularDatabase extends Migration
                 [
                     'id' => 1,
                     'nome' => 'Cultura Sudoeste',
-                    'dt_ini' => '01/01/2019',
-                    'dt_fim' => '22/01/2022',
+                    'dt_ini' => '01-01-2019',
+                    'dt_fim' => '22-01-2022',
                     'tipo' => 'CANAVIAL',
                     'ha' => '10',
                     'custo' => '12000',
@@ -109,8 +109,8 @@ class PopularDatabase extends Migration
                 [
                     'id' => 1,
                     'nome' => 'Pastagem inicial',
-                    'dt_ini' => '22/02/2021',
-                    'dt_fim' => '05/12/2021',
+                    'dt_ini' => '22-02-2021',
+                    'dt_fim' => '05-12-2021',
                     'area' => '33',
                     'tipo' => 'ANUAL',
                     'custo' => '9000',
@@ -168,6 +168,63 @@ class PopularDatabase extends Migration
                 ]
             ]
         );
+        DB::table('animais')->insert(
+            [
+                [
+                    'id' => 1,
+                    'video' => 'https://www.youtube.com/watch?v=ibw_sUAqyeQ&ab_channel=CentraldeFilmeseS%C3%A9ries',
+                    'imagem_id' => 1,
+                    'nome' => 'Brabo',
+                    'sexo' => 'MACHO',
+                    'sangue' => '1/2',
+                    'raca' => 'ANGUS',
+                    'brinco' => 12,
+                    'origem' => 'COMPRA',
+                    'dt_nasc' => '19-08-2017',
+                    'peso' => '1235',
+                    'nome_reg' => 'Baseado',
+                    'num_reg' => '1',
+                    'raca_2' => 'GIR',
+                    'pelagem' => 'Clara',
+                    'dt_entrada' => '21-08-22',
+                    'peso_entrada' => '1250',
+                    'lote_id' => '1',
+                    'fornecedor_id' => '1',
+                    'ativo' => '1',
+                    'created_at' => '2022-08-03 16:55:45',
+                    'updated_at' => '2022-08-03 16:55:45'
+                ]
+            ]
+        );
+
+        DB::table('animais')->insert(
+            [
+                [
+                    'id' => 2,
+                    'video' => 'https://www.youtube.com/watch?v=ibw_sUAqyeQ&ab_channel=CentraldeFilmeseS%C3%A9ries',
+                    'imagem_id' => 2,
+                    'nome' => 'Braba',
+                    'sexo' => 'FEMEA',
+                    'sangue' => '1/4',
+                    'raca' => 'ANELORADO',
+                    'brinco' => 22,
+                    'origem' => 'COMPRA',
+                    'dt_nasc' => '09-01-2018',
+                    'peso' => '1120',
+                    'nome_reg' => 'Mimosa',
+                    'num_reg' => '2',
+                    'raca_2' => 'GIR LEITEIRO',
+                    'pelagem' => 'Escura',
+                    'dt_entrada' => '21-08-22',
+                    'peso_entrada' => '1176',
+                    'lote_id' => '1',
+                    'fornecedor_id' => '1',
+                    'ativo' => '1',
+                    'created_at' => '2022-08-03 16:55:45',
+                    'updated_at' => '2022-08-03 16:55:45'
+                ]
+            ]
+        );
     }
 
     /**
@@ -205,6 +262,9 @@ class PopularDatabase extends Migration
             //
         });
         Schema::table('lotes', function (Blueprint $table) {
+            //
+        });
+        Schema::table('animais', function (Blueprint $table) {
             //
         });
     }
