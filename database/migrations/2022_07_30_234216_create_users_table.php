7d82c4e1b6f8f843de59a015c7bf6b6af69285ca
Joao Queiroz <joaovsq@hotmail.com>
2023-03-02 08:32:17 -0300

    Revert ":construction:feat imagem de usuário não pode ser null"
    This reverts commit e4b514d245dd0debff971ad00402e5389272c5fb.

--- database/migrations/2022_07_30_234216_create_users_table.php
+++ database/migrations/2022_07_30_234216_create_users_table.php
@@ -15,7 +15,7 @@ class CreateUsersTable extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('imagem')->nullable()->default('');
+            $table->string('imagem')->default('');
             $table->string('name');
             $table->string('email')->unique();
             $table->timestamp('email_verified_at')->nullable();
