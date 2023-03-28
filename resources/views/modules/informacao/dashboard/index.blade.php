25b16cc9372c9d2f94166458eddb72c52e293f62
Gabriel <gabrieldias122@gmail.com>
2023-03-27 22:20:58 -0300

    atualizações diárias

--- resources/views/modules/informacao/dashboard/index.blade.php
+++ resources/views/modules/informacao/dashboard/index.blade.php
@@ -164,6 +164,8 @@
             <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
             <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
 
+            {{-- Grafico de animais por raça --}}
+
             <script type="text/javascript">
                 var analytics = <?php echo $raca; ?>
 
